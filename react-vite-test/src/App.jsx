5f5180405ed3a0807ef1985e6bd01f5b5f868b17
Benjaminoyarzun <benjaminoyarzunlopez@iresm.edu.ar>
2023-09-28 21:08:33 -0300

    terminado

---
+++ react-vite-test/src/App.jsx
@@ -0,0 +1,24 @@
+
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1> Este es mi testeo de React+Vite con mis manga favoritos </h1>
+      <ol>
+        <li>JJK</li>
+        <li>Naruto</li>
+        <li>Vagabond</li>
+        <li>Berserk</li>
+        <li>DBZ</li>
+
+
+      </ol>
+     
+    </div>
+  );
+};
+ 
+
+export default App;
